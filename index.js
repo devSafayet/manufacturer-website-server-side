7659bfc0110499e3acedd3f886547fde0e4202af
Safayet <mrishafayet003@gmail.com>
2022-05-28 03:24:15 +0600

    added get all products

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3bymp.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 /* client.connect(err => {
@@ -17,11 +19,39 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
     // perform actions on the collection object
     client.close();
 }); */
+
 async function run() {
     try {
         await client.connect()
-        const collection = client.db("test").collection("devices");
-        client.close();
+        const bikeToolsCollection = client.db("motor-bike-tools").collection("services");
+
+
+
+        // get all products
+
+        app.get('/services', async (req, res) => {
+            const count = await bikeToolsCollection.estimatedDocumentCount();
+            res.send({ count })
+        });
+        app.get('/services', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const query = {}
+            const cursor = bikeToolsCollection.find(query);
+
+            let products;
+
+            if (page || size) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+
+
+            res.send(services)
+        })
 
     }
     finally {
