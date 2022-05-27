eb903b24db4d1fff3338895e6f1cf8ff2a78e057
Safayet <mrishafayet003@gmail.com>
2022-05-27 11:05:15 +0600

    middleware added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Server mama cholteche')
