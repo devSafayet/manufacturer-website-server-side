1856ec459f3cdf75d2ce0eee7ea056c7497a2956
Safayet <mrishafayet003@gmail.com>
2022-05-28 03:07:14 +0600

    mongodb connections

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3bymp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+/* client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+}); */
+async function run() {
+    try {
+        await client.connect()
+        const collection = client.db("test").collection("devices");
+        client.close();
+
+    }
+    finally {
+
+    }
+}
+
+
 app.get('/', (req, res) => {
     res.send('Server mama cholteche')
 });
