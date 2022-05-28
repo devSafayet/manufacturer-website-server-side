13b238323804c4bf9a43879a7ee033e4ac5cce94
Safayet <mrishafayet003@gmail.com>
2022-05-28 06:57:13 +0600

    added spacific order

--- index.js
+++ index.js
@@ -166,7 +166,13 @@ async function run() {
             const result = await orderCollection.find({ email }).toArray()
             res.send(result)
         });
-
+        // get specific order
+        app.get('/specificorders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const order = await orderCollection.findOne(query);
+            res.send(order)
+        });
 
     }
     finally {
